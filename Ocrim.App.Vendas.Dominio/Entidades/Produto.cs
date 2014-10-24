82b12671e5b6506139f57322bbac09c91f30afd8
heberolimpio <heber.olimpio@gmail.com>
2014-10-24 19:46:35 -0200

    Segundo Commit

---
+++ Ocrim.App.Vendas.Dominio/Entidades/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocrim.App.Vendas.Dominio.Entidades
+{
+    class Produto //classe para cuidar dos produtos
+    {
+        
+        //propriendades da classe produto
+
+        public int ProdutoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string Categoria { get; set; }
+    }
+}
